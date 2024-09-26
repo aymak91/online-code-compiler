bf650467b53a9eca9fe7ab93fe9a41e7c85d00d5
FrancisHuynh95 <huynh_francis95@icloud.com>
2024-09-26 16:35:43 -0700

    creating splash page

---
+++ components/SplashPage.tsx
@@ -0,0 +1,10 @@
+"use-client"
+
+export default function SplashPage() {
+    return (
+        <div>
+            <h1>Splash Page</h1>
+            <button>Login button</button>
+        </div>
+    )
+}
